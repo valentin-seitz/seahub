3fa6ed00b4dfcccf808c2883029ea6f40e46966b
lian <imwhatiam123@gmail.com>
2019-03-01 18:23:01 +0800

    [alibaba custom] new download client page

--- frontend/src/components/side-nav-footer.js
+++ frontend/src/components/side-nav-footer.js
@@ -21,10 +21,6 @@ class SideNavFooter extends React.Component {
       <div className="side-nav-footer">
         <a href={siteRoot + 'help/'} target="_blank" rel="noopener noreferrer" className="item">{gettext('Help')}</a>
         <a className="item" onClick={this.onAboutDialogToggle}>{gettext('About')}</a>
-        <a href={siteRoot + 'download_client_program/'} className="item last-item">
-          <span aria-hidden="true" className="sf2-icon-monitor vam"></span>{' '}
-          <span className="vam">{gettext('Clients')}</span>
-        </a>
         {this.state.isAboutDialogShow &&
           <ModalPortal>
             <AboutDialog onCloseAboutDialog={this.onAboutDialogToggle} />
