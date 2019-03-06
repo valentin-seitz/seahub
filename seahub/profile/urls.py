dcf8bb521f502f0a7e3cd7d477738b3f12241dcd
lian <imwhatiam123@gmail.com>
2019-03-06 15:58:25 +0800

    [alibaba custom] new profile page

--- seahub/profile/urls.py
+++ seahub/profile/urls.py
@@ -2,9 +2,11 @@
 from django.conf.urls import url, include
 from .views import *
 
+from seahub.alibaba.views import alibaba_edit_profile, alibaba_user_profile
+
 urlpatterns = [
 #    url(r'^list_user/$', 'list_userids', name="list_userids"),
-    url(r'^$', edit_profile, name="edit_profile"),
+    url(r'^$', alibaba_edit_profile, name="edit_profile"),
     url(r'^(?P<user>[^/]+)/get/$', get_user_profile, name="get_user_profile"),
     url(r'^delete/$', delete_user_account, name="delete_user_account"),
     url(r'^default-repo/$', default_repo, name="default_repo"),
@@ -13,5 +15,5 @@ urlpatterns = [
 
 # Move the catch-all pattern to the end.
 urlpatterns += [
-    url(r'^(?P<username>[^/]*)/$', user_profile, name="user_profile"),
+    url(r'^(?P<username>[^/]*)/$', alibaba_user_profile, name="user_profile"),
 ]
